3095b6f8465a7f0b30a2407c5a34734a914ea730
Ryan Parsley <Ryan.Parsley@JBHunt.com>
2024-02-28 17:00:25 -0500

    feat: stub out a note-detail page

--- libs/note/src/lib/lib.routes.ts
+++ libs/note/src/lib/lib.routes.ts
@@ -1,4 +1,8 @@
 import { Route } from '@angular/router';
 import { NoteComponent } from './note/note.component';
+import { NoteDetailComponent } from './note-detail/note-detail.component';
 
-export const noteRoutes: Route[] = [{ path: '', component: NoteComponent }];
+export const noteRoutes: Route[] = [
+  { path: '', component: NoteComponent },
+  { path: ':id', component: NoteDetailComponent }
+];
